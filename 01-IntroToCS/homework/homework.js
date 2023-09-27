c4bf1635a3f49421497f65940e42c3e1f9b8db30
Raul <ccontrerasvelasco@gmail.com>
2023-09-26 18:28:07 -0600

    Homework 1 resuelta

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,45 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   
+   let resultado = 0;
+   let j = 0;
+    
+   for(let i = num.length - 1; i >= 0 ; i--) {        
+      if(num[i] === '1') {            
+         resultado += Math.pow(2, j);            
+         j++
+      } else {
+         j++
+      }
+   }
 
-function DecimalABinario(num) {}
+   return resultado;
+
+   // num = num.split('').reverse();
+   // let resultado = 0;
+   
+   // for(let i = 0; i < num.length; i++) {
+   //    resultado += Math.pow(2, i) * num[i];      
+   // }
+
+   // return resultado;
+}
+
+function DecimalABinario(num) {
+
+   let binario = '';
+
+   while ( num > 0 ) {
+      const residuo = num % 2;
+      binario = residuo.toString() + binario;
+      num = Math.floor(num / 2);
+   }
+
+   return binario;
+   // return num.toString(2);
+}
+
 
 module.exports = {
    BinarioADecimal,
